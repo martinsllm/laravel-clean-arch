16dbbeb3db6e79ba327758d861d0c64cf6262152
martinsllm <lucaslm2010@gmail.com>
2025-08-02 13:20:56 -0300

    Setting up tests

---
+++ mini-leanpub/tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\MiniLeanpub;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
